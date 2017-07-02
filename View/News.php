7acc773417aff17a275e492e45ef959c35e2eb62
yaobosan <yaobosan@gmail.com>
2017-07-03 06:58:21 +0700

    Add files via upload

---
+++ View/News.php
@@ -0,0 +1,34 @@
+<html>
+<?php
+	include "../Include/header.php";
+	?>
+	<link rel="stylesheet" type="text/css" href="../css/news.css">
+<body>
+	<div class="Announce">
+		<div class="backAnnounce">
+			&nbsp
+		</div>
+		<div class="contentAnnounce">
+			<div class="titleAnnounce">
+				Featured News
+			</div>
+			<div class="contentTitle">
+				<pre>
+Title Of The News Dolor Sit Amet
+				</pre>
+			</div>
+			<div class="dateAnnounce">
+				<i class="fa fa-calendar"></i> 19 Jan 2017 | 12:51
+			</div>
+			<ol class="bulletAnnounce">
+					<li></li>
+					<li style="background-color:white;"></li>
+					<li></li>
+			</ol>
+		</div>
+	</div>
+<?php
+	include "../Include/footer.php";
+?>
+</body>
+</html>		
